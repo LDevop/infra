0a6c7efb5468fc11c29b2a42ab324770b9f744c2
Denis Loban <xxx@zzz.yyy>
2022-06-10 17:19:45 +0400

    slave

--- backend.tf
+++ backend.tf
@@ -1,5 +1,14 @@
+# terraform {
+#   backend "local" {
+#     path = "/home/ubuntu/tfstate/terraform.tfstate"
+#   }
+# }
+
 terraform {
-  backend "local" {
-    path = "/home/ubuntu/tfstate/terraform.tfstate"
+  backend "s3" {
+    bucket = "tfstate-for-jenkins"
+    key    = "jenkins/terraform.tfstate"
+    region = "eu-central-1"
   }
+
 }
