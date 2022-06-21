de75e48bc59eb763124f88a5333f809001a41201
Denis Loban <xxx@zzz.yyy>
2022-06-21 21:49:56 +0400

    edited r53

--- route52-ecs.tf
+++ route52-ecs.tf
@@ -1,16 +1,15 @@
-data "aws_route53_zone" "r53_zone" {
-  name         = "pupkinsite.boloto.com."
-  private_zone = false
+resource "aws_route53_zone" "r53_zone" {
+  name = "pupkinsite.boloto.com."
 }
 
 resource "aws_route53_record" "dns" {
-  zone_id = data.aws_route53_zone.r53_zone.zone_id
-  name    = "www.${data.aws_route53_zone.r53_zone.name}"
+  zone_id = aws_route53_zone.r53_zone.zone_id
+  name    = "www.${aws_route53_zone.r53_zone.name}"
   type    = "A"
 
   alias {
     evaluate_target_health = false
-    name                   = "${aws_alb.alb.dns_name}"
-    zone_id                = "${aws_alb.alb.zone_id}"
+    name                   = aws_alb.alb.dns_name
+    zone_id                = aws_alb.alb.zone_id
   }
 }
