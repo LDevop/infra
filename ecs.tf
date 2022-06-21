8f04602fe3be27cdd875a1e3054827011c268319
Denis Loban <xxx@zzz.yyy>
2022-06-21 15:03:15 +0400

    delete healtchek

--- ecs.tf
+++ ecs.tf
@@ -50,7 +50,7 @@ resource "aws_ecs_service" "ecs-service" {
   scheduling_strategy                = "REPLICA"
   deployment_minimum_healthy_percent = "50"
   deployment_maximum_percent         = "200"
-  health_check_grace_period_seconds  = "5"
+  #health_check_grace_period_seconds  = "5"
   force_new_deployment               = true
 
   ordered_placement_strategy {
